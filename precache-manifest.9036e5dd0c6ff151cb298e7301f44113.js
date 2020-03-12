e60d803a675dc573e187b37a65671c7e01019b9c
zoezachary <zoezachary27@gmail.com>
2020-03-12 15:24:34 +0700

    Updates

---
+++ precache-manifest.9036e5dd0c6ff151cb298e7301f44113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751de8c2f780e83f7dec36fce67da825",
+    "url": "/index.html"
+  },
+  {
+    "revision": "513e5204cf7b2cc4f6a9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c11091d1589b095374f3",
+    "url": "/static/js/2.ce001945.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.ce001945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "513e5204cf7b2cc4f6a9",
+    "url": "/static/js/main.2f0b95d5.chunk.js"
+  },
+  {
+    "revision": "17f7ae0025ca5601e3f8",
+    "url": "/static/js/runtime-main.8cc5da75.js"
+  }
+]);
