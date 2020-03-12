ed1e0aa196140546d0624947f644be25b734a9c4
zoezachary <zoezachary27@gmail.com>
2020-03-12 15:33:37 +0700

    Adding docs

---
+++ docs/precache-manifest.4effd4fe1e5ca91a6d6fe61f06bcf43e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8568e894dfeba924ceaf0580db94ab35",
+    "url": "/fabelio-frontend-test/index.html"
+  },
+  {
+    "revision": "c44c752e1c0d39fc5557",
+    "url": "/fabelio-frontend-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c11091d1589b095374f3",
+    "url": "/fabelio-frontend-test/static/js/2.ce001945.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/fabelio-frontend-test/static/js/2.ce001945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c44c752e1c0d39fc5557",
+    "url": "/fabelio-frontend-test/static/js/main.d76e9747.chunk.js"
+  },
+  {
+    "revision": "c4b2de275ab89884d7c2",
+    "url": "/fabelio-frontend-test/static/js/runtime-main.13ea191f.js"
+  }
+]);
